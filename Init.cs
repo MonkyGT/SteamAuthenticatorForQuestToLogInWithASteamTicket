16bb8bbb8eae46d7e84657ed5884dcc96c1b2c62
MonkyGT <monkygtstudios@gmail.com>
2025-06-27 21:51:32 -0400

    Initial commit

---
+++ Init.cs
@@ -0,0 +1,17 @@
+﻿using MelonLoader;
+using SteamAuthenticatorForQuestToLogInWithASteamTicket;
+
+[assembly: MelonInfo(typeof(MelonLoaderInit), ModInfo.Name, ModInfo.Version, ModInfo.Author)]
+[assembly: MelonGame("Duttbust", "Capuchin")]
+
+namespace SteamAuthenticatorForQuestToLogInWithASteamTicket
+{
+    public class ModInfo
+    {
+        public const string UUID = "yourname.yourmod";
+        public const string Name = "Mod";
+        public const string Author = "Your Name";
+        public const string Version = "1.0.0";
+    }
+    public class MelonLoaderInit : MelonMod;
+}
---
+++ Patches/Patches.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using HarmonyLib;
+using Il2Cpp;
+using Il2CppCapuchinPlayFab;
+using Il2CppPlayFab;
+using Il2CppPlayFab.ClientModels;
+using Il2CppSystem.Threading.Tasks;
+using MelonLoader.Utils;
+
+namespace SteamAuthenticatorForQuestToLogInWithASteamTicket.Patches
+{
+    [HarmonyPatch(typeof(LoginManager), nameof(LoginManager.StartLoginProcess))]
+    public class StartLoginProcessPatch
+    {
+        public static bool Prefix(LoginManager __instance)
+        {
+            var authTicketPath = Path.Combine(MelonEnvironment.UserDataDirectory, "steam_ticket.txt");
+
+            if (!File.Exists(authTicketPath))
+            {
+                return true;
+            }
+
+            Il2CppSystem.Collections.Generic.Dictionary<string, string> dictionary = new();
+            dictionary.Add("MetaId", "STEAM");
+            dictionary.Add("MetaAuth", "STEAM");
+
+            string authTicket = File.ReadAllText(authTicketPath);
+            LR_DebugMenu.instance.debugValuePairs.Add("loginAttempted", true);
+
+            var loginRequest = new LoginWithSteamRequest
+            {
+                SteamTicket = authTicket,
+                CreateAccount = new Il2CppSystem.Nullable<bool>(false),
+                CustomTags = dictionary,
+                InfoRequestParameters = new GetPlayerCombinedInfoRequestParams
+                {
+                    GetUserInventory = true,
+                    GetUserVirtualCurrency = true,
+                    GetTitleData = true,
+                    GetPlayerProfile = true
+                }
+            };
+            System.Action<LoginResult> onSuccess = __instance.OnLogIn;
+            System.Action<PlayFabError> onError = __instance.OnPlayFabError;
+
+            PlayFabClientAPI.LoginWithSteam(loginRequest, onSuccess, onError);
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(FusionHub), nameof(FusionHub.GetNonce))]
+    public class GetNoncePatch
+    {
+        static bool Prefix(ref Task<string> __result)
+        {
+            __result = Task.FromResult("STEAM");
+            return false;
+        }
+    }
+}
